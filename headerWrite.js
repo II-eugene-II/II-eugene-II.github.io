4c8b51450bc6bca2916d2bfb6d65beb243e21fef
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2022-08-02 20:04:54 +0900

    Update headerWrite.js

--- headerWrite.js
+++ headerWrite.js
@@ -1,12 +1,8 @@
-document.write("<h1>이것은 write함수 실습입니다.</h1>");
-document.write("<h2>II-eugene-II</h2>");
 document.write("<nav>");
 document.write("<ul>");
-document.write("<h3>II-eugene-II</h3>");
-document.write("<h3>III-eugene-III</h3>");
-//document.write("<li><a href="#">Home</a></li>");
-//document.write("<li><a href="#">Math</a></li>");
-//document.write("<li><a href="#">Code</a></li>");
-//document.write("<li><a href="#">Support</a></li>");
+document.write("<li><a href=\"#\">Home</a></li>");
+document.write("<li><a href=\"#\">Math</a></li>");
+document.write("<li><a href=\"#\">Code</a></li>");
+document.write("<li><a href=\"#\">Support</a></li>");
 document.write("</ul>");
 document.write("</nav>");
