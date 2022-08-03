948bebbc42ed5e08185baa104b1f85de8dca6983
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2022-08-03 18:08:54 +0900

    Update headerWrite.js

--- headerWrite.js
+++ headerWrite.js
@@ -1,9 +1,9 @@
 document.write("<h2>II-eugene-II</h2>");
 document.write("<nav>");
 document.write("<ul>");
-document.write("<li><a href=\"/index.html\">Home</a></li>");
-document.write("<li><a href=\"#\">Math</a></li>");
-document.write("<li><a href=\"#\">Code</a></li>");
-document.write("<li><a href=\"#\">Support</a></li>");
+document.write("<li><a class=\"header-item\" href=\"/index.html\">Home</a></li>");
+document.write("<li><a class=\"header-item\" href=\"#\">Math</a></li>");
+document.write("<li><a class=\"header-item\" href=\"#\">Code</a></li>");
+document.write("<li><a class=\"header-item\" href=\"/history.html\">History</a></li>");
 document.write("</ul>");
 document.write("</nav>");
