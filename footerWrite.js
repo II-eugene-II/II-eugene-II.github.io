53b082143f25a8fd72d68f4a184869ab17a4b813
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2022-08-03 20:31:00 +0900

    Update footerWrite.js

--- footerWrite.js
+++ footerWrite.js
@@ -1,4 +1,4 @@
 document.write("<nav>");
-document.write("<a href=\"/index.html\">Home</a>");
-document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\">Github</a>");
+document.write("<a href=\"/index.html\">Home</a> ");
+document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\">GithubRepo</a>");
 document.write("</nav>");
