3fc0f062170b5e33d365871ae99f73e0fd5c2f48
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2022-08-03 20:09:49 +0900

    Update headerWrite.js

--- headerWrite.js
+++ headerWrite.js
@@ -5,5 +5,5 @@ document.write("<li class=\"header-item\"><a href=\"/index.html\">Home</a></li>"
 document.write("<li class=\"header-item\"><a href=\"#\">Math</a></li>");
 document.write("<li class=\"header-item\"><a href=\"#\">Code</a></li>");
 document.write("<li class=\"header-item\"><a href=\"/history.html\">History</a></li>");
-document.write("<ul class=\"header-ull">");
+document.write("</ul>");
 document.write("</nav>");
