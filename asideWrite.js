8d3e6a3f92ca6fa33e34e12f20da02c6249796fc
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2022-08-22 19:38:56 +0900

    Update asideWrite.js

--- asideWrite.js
+++ asideWrite.js
@@ -1,3 +1,3 @@
 document.write("a<br>s<br>i<br>d<br>e<br>");
-document.write("<a href=\"/index.html\">Home</a> ");
-document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\">GithubRepo</a>");
+document.write("<a href=\"/index.html\"> Home </a> ");
+document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\"> Github Repo </a>");
