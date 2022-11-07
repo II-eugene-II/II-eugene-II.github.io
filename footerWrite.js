17cbc4e1ac942a20859efccb5859f0f09d156990
II-eugene-II <214792@jnu.ac.kr>
2022-11-07 17:01:32 +0900

    new 2 Post(33-34) + etc

--- footerWrite.js
+++ footerWrite.js
@@ -1,4 +1,5 @@
 document.write("<nav>");
 document.write("<a href=\"/index.html\">Home</a> ");
-document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\">GithubRepo</a>");
+document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\">GithubRepo</a><br>");
+document.write("II-eugene-II note");
 document.write("</nav>");
