c99a658e0b644d4af0992229f10889a0900d266e
II-eugene-II <214792@jnu.ac.kr>
2022-11-21 17:39:51 +0900

    random Page

--- asideWrite.js
+++ asideWrite.js
@@ -1,3 +1,16 @@
-document.write("a<br>s<br>i<br>d<br>e<br>160<br>");
+maxPost = 57
+
+rand = Math.floor(Math.random() * maxPost) + 1
+
+rand = rand.toString()
+
+
+
+document.write(rand);
+
+document.write("a<br>s<br>i<br>d<br>e<br>165<br>");
 document.write("<a href=\"/index.html\"> Main Page </a> ");
 document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\"> Github Repo </a>");
+document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Main Page </a> ");
+
+// a href="/Post/Post01000/00011.html"
