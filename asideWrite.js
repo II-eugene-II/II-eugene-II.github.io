714d7f6f012fc5e82074dd3019472e83d7e13d11
II-eugene-II <214792@jnu.ac.kr>
2022-11-21 20:35:22 +0900

    etc

--- asideWrite.js
+++ asideWrite.js
@@ -4,13 +4,7 @@ rand = Math.floor(Math.random() * maxPost) + 1
 
 rand = rand.toString()
 
-
-
-document.write(rand);
-
-document.write("a<br>s<br>i<br>d<br>e<br>165<br>");
+document.write("a<br>s<br>i<br>d<br>e<br>160<br>");
 document.write("<a href=\"/index.html\"> Main Page </a> ");
 document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\"> Github Repo </a>");
-document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Main Page </a> ");
-
-// a href="/Post/Post01000/00011.html"
+document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Random Page </a> ");
