5b5d18ebb6f70a4e992045e80d2892d98510d214
II-eugene-II <214792@jnu.ac.kr>
2022-11-25 17:04:00 +0900

    google Analytics Test 2

---
+++ googleAnalytics.js
@@ -0,0 +1,10 @@
+document.write(`
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S21Q60B9JB"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-S21Q60B9JB');
+</script>
+`)
