873d437834ba831144fae8aa78a5b152d0495c33
II-eugene-II <214792@jnu.ac.kr>
2022-11-25 20:34:39 +0900

    google tracking + media Query (v.0.1.0)

--- asideWrite.js
+++ asideWrite.js
@@ -6,5 +6,7 @@ rand = rand.toString()
 
 document.write("a<br>s<br>i<br>d<br>e<br>160<br>");
 document.write("<a href=\"/index.html\"> Main Page </a> ");
+document.write("<br>");
 document.write("<a href=\"https://github.com/II-eugene-II/II-eugene-II.github.io\"> Github Repo </a>");
+document.write("<br>");
 document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Random Page </a> ");
