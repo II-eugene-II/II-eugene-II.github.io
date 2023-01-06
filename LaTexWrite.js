71d4f15a486b7a6671302a4cb83216608c8107f2
II-eugene-II <214792@jnu.ac.kr>
2023-01-06 22:33:53 +0900

    New 1 Post(116) + etc(100 정수론 계산기 가까운 소수 찾기 기능 추가)

--- LaTexWrite.js
+++ LaTexWrite.js
@@ -1,3 +1,3 @@
-document.write("<script src=\"//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\">");
-document.write("MathJax.Hub.Config({tex2jax: { inlineMath: [[\'$\', \'$\']], displayMath: [[\'$$\', \'$$\']] }});");
-document.write("</script>");
+document.write(`<script src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+MathJax.Hub.Config({tex2jax: { inlineMath: [['$', '$']], displayMath: [['$$', '$$']] }});
+</script>`);
