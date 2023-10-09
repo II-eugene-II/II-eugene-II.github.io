f3283bcfc387ad84090acb8190c49f91eaa25384
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2023-10-09 14:52:53 +0900

    Update footerWrite.js
    맨 밑까지 내렸으면 랜덤 페이지를 먼저 보려하지 않을까?

--- footerWrite.js
+++ footerWrite.js
@@ -4,12 +4,14 @@ rand = Math.floor(Math.random() * maxPost) + 1
 
 rand = rand.toString()
 
+document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Random Page </a> ");
+
 document.write(`
 <nav>
-    <a href="/index.html">Home</a>
+    <a href="/index.html"> Home </a>
     <a href="https://github.com/II-eugene-II/II-eugene-II.github.io"> Github Repo </a> <br>
 `)
-document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Random Page </a> ");
+
 document.write(`
 II-eugene-II note
 </nav>
