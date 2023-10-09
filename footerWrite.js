cb0af3010e30f99364ddf49327ca84789b565423
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2023-10-09 15:04:11 +0900

    Update footerWrite.js
    페이지 이름 맨 위로

--- footerWrite.js
+++ footerWrite.js
@@ -4,15 +4,15 @@ rand = Math.floor(Math.random() * maxPost) + 1
 
 rand = rand.toString()
 
+document.write(`
+II-eugene-II note <br>
+`)
+
 document.write("<a href=\"/Post/Post01000/" + rand.padStart(5, "0") + ".html\"> Random Page </a> ");
 
 document.write(`
 <nav>
-    <a href="/index.html"> Home </a>
+    <a href="/index.html"> Home </a> <br>
     <a href="https://github.com/II-eugene-II/II-eugene-II.github.io"> Github Repo </a> <br>
-`)
-
-document.write(`
-II-eugene-II note
 </nav>
 `)
