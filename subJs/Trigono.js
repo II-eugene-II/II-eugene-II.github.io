fcadc4384c57394ec8956e9410c7d0285f6220fb
II-eugene-II <104616990+II-eugene-II@users.noreply.github.com>
2024-04-30 19:12:39 +0900

    Create Trigono.js
    삼각함수 계열 함수 모음

---
+++ subJs/Trigono.js
@@ -0,0 +1,81 @@
+document.write(`
+<details> <summary> 기초 삼각함수 </summary>
+        
+    <p>
+    
+        <a href="/Post/Post01000/00184.html"> 사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00185.html"> 코사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00227.html"> 탄젠트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00239.html"> 코시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00241.html"> 시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00242.html"> 코탄젠트함수 </a> <br>
+        
+    </p>
+        
+</details>
+
+<details> <summary> 역삼각함수 </summary>
+        
+    <p>
+    
+        <a href="/Post/Post01000/00311.html"> 아크사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00312.html"> 아크코사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00313.html"> 아크탄젠트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00314.html"> 아크코시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00315.html"> 아크시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00316.html"> 아크코탄젠트함수 </a> <br>
+        
+    </p>
+        
+</details>
+
+<details> <summary> 쌍곡선함수 </summary>
+        
+    <p>
+    
+        <a href="/Post/Post01000/00236.html"> 쌍곡사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00237.html"> 쌍곡코사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00238.html"> 쌍곡탄젠트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00243.html"> 쌍곡코시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00244.html"> 쌍곡시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00253.html"> 쌍곡코탄젠트함수 </a> <br>
+        
+    </p>
+        
+</details>
+
+<details> <summary> 역쌍곡선함수 </summary>
+        
+    <p>
+    
+        <a href="/Post/Post01000/00367.html"> 아크쌍곡사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00368.html"> 아크쌍곡코사인함수 </a> <br>
+        
+        <a href="/Post/Post01000/00369.html"> 아크쌍곡탄젠트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00370.html"> 아크쌍곡코시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00371.html"> 아크쌍곡시컨트함수 </a> <br>
+        
+        <a href="/Post/Post01000/00372.html"> 아크쌍곡코탄젠트함수 </a> <br>
+        
+    </p>
+        
+</details>
+`)
