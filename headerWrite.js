17165d533b147b9e1202864103f1782bd5dc0540
II-eugene-II <214792@jnu.ac.kr>
2024-07-02 22:53:29 +0900

    디자인 추가 { header 천장에 고정 blockquote 그림자로 인한 내용 침범으로 인해 margin 추가 형광펜 강조 기능 추가 (highlighter-html) } 그 외 내용 보강 등

--- headerWrite.js
+++ headerWrite.js
@@ -1,11 +1,16 @@
-document.write("<div class=\"container\">");
-document.write("<h2>II-eugene-II Note</h2>");
-document.write("<nav>");
-document.write("<ul class=\"header-ull\">");
-document.write("<li class=\"header-item\"><a href=\"/index.html\">Home</a></li>");
-document.write("<li class=\"header-item\"><a href=\"/Math/math.html\">Math</a></li>");
-document.write("<li class=\"header-item\"><a href=\"/Code/code.html\">Code</a></li>");
-document.write("<li class=\"header-item\"><a href=\"/history.html\">History</a></li>");
-document.write("</ul>");
-document.write("</nav>");
-document.write("</div>");
+document.write(`
+<div class="container">
+    <h2 style="line-height:150%;">II-eugene-II Note</h2>
+    <a class="header-item" href="/index.html">Home</a> <a class="header-item" href="/Math/math.html">Math</a> <a href="/Code/code.html">Code</a> <br>
+    <!-- 
+    <nav>
+        <ul class="header-ull">
+            <li class="header-item"><a href="/index.html">Home</a></li>
+            <li class="header-item"><a href="/Math/math.html">Math</a></li>
+            <li class="header-item"><a href="/Code/code.html">Code</a></li>
+            <li class="header-item"><a href="/history.html">History</a></li> 
+        </ul>
+    </nav>
+    -->
+</div>
+`)
